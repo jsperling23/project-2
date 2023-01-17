4f92ddcb340bf94f9910571f91ded109157323c9
jsperling23 <joefish612@yahoo.com>
2023-01-16 16:14:43 -0800

    Initial commit

---
+++ project-2a.py
@@ -0,0 +1,9 @@
+print("Please enter five numbers.")
+x1=float(input())
+x2=float(input())
+x3=float(input())
+x4=float(input())
+x5=float(input())
+avg=(x1+x2+x3+x4+x5)/5
+print("The average of those numbers is: ")
+print(avg)
